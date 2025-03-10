bb87e32e01b2bdfa0c6f24e3ec6cd3d42480c59c
Dashadower <tttllshin@gmail.com>
2025-03-10 11:44:03 +0000

    work on stlc

--- plf/Stlc.v
+++ plf/Stlc.v
@@ -751,13 +751,23 @@ Lemma step_example5 :
        <{idBBBB idBB idB}>
   -->* idB.
 Proof.
-  (* FILL IN HERE *) Admitted.
+  eapply multi_step.
+  - apply ST_App1. apply ST_AppAbs. apply v_abs.
+  - eapply multi_step.
+    + apply ST_AppAbs. apply v_abs.
+    + simpl. apply multi_refl.
+Qed.
 
 Lemma step_example5_with_normalize :
        <{idBBBB idBB idB}>
   -->* idB.
 Proof.
-  (* FILL IN HERE *) Admitted.
+  eapply multi_step.
+  - apply ST_App1. apply ST_AppAbs. apply v_abs.
+  - eapply multi_step.
+    + apply ST_AppAbs. apply v_abs.
+    + simpl. apply multi_refl.
+Qed.
 (** [] *)
 
 (* ################################################################# *)
